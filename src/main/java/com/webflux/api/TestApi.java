9b9b6d9e31817ce9be649fb91a24c5cf86d8e8cd
Prasad Pansare <Prasad Pansare@HANDT.AI>
2026-04-07 19:44:47 +0530

    GitHub Workflows Fix 1

--- src/main/java/com/webflux/api/TestApi.java
+++ src/main/java/com/webflux/api/TestApi.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/public")
 @RequiredArgsConstructor
+@Slf4j
 public class TestApi {
 
 	@GetMapping
 	public String testFlow() {
+		log.info("running server");
 		return "hello from server";
 	}
 
